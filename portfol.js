8e6e5e6801a9ea47a95cab1388dde79f6465b62c
Shreya-103 <shreyaalpha1@gmail.com>
2026-03-19 11:57:08 +0530

    updated menu setting

--- portfol.js
+++ portfol.js
@@ -4,3 +4,10 @@ const sidebar = document.querySelector("#sidebar");
 function toggleSidebar(){
     sidebar.classList.toggle("active");
 }
+
+const body = document.body;
+body.addEventListener('click', (e)=>{
+    if(!sidebar.contains(e.target)){
+        sidebar.classList.remove("active");
+    }
+});
