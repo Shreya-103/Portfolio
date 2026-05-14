7d4fc6ef56b9b8614397a1c8e243026147c7c606
Shreya-103 <shreyaalpha1@gmail.com>
2026-05-14 23:03:48 +0530

    js format

--- portfol.js
+++ portfol.js
@@ -18,17 +18,11 @@ const aboutMe = document.querySelector('#aboutMe');
 
 function changeMood(){
     if(mood.innerHTML === '🌙'){
-            document.body.classList.add("dark");
-        // document.querySelector("body").style.background = "black";
-        // aboutMe.style.background = "black";
-        // aboutMe.style.color = "white";
+        document.body.classList.add("dark");
         mood.innerHTML = "🔆";
     }
     else{
-            document.body.classList.remove("dark");
-        // document.querySelector("body").style.background = "#ffff";
-        // aboutMe.style.background = "linear-gradient(-45deg, #ff9a9e, #fad0c4, #a1c4fd, #c2e9fb)";
-        // aboutMe.style.color = "black";
+        document.body.classList.remove("dark");
         mood.innerHTML = "🌙";
 
     }
